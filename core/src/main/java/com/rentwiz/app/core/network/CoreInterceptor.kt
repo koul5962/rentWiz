0a6c2f60d2df6114b644b9ecd44e10581211aefa
Sunil Koul <sunil.koul@incred.com>
2024-01-04 11:30:53 +0530

    Set up git actions workflow

--- core/src/main/java/com/rentwiz/app/core/network/CoreInterceptor.kt
+++ core/src/main/java/com/rentwiz/app/core/network/CoreInterceptor.kt
@@ -7,6 +7,7 @@ import javax.inject.Inject
 class CoreInterceptor @Inject constructor(): Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
+
         request.addHeader("Authorization","token")
         return chain.proceed(request.build())
     }
